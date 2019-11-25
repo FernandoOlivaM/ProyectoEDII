f1b2ae7a13ddde339d5fa86c25dfd7e8331b223c
feroliv4z <feroliv4z@gmail.com>
2019-11-24 18:43:27 -0600

    corrección al recibir usuario y contraseña en login

--- ProyectoEDII/TeleCord/Controllers/LogInController.cs
+++ ProyectoEDII/TeleCord/Controllers/LogInController.cs
@@ -8,9 +8,17 @@ namespace TeleCord.Controllers
 {
     public class LogInController : Controller
     {
-        // GET: LogIn
         public ActionResult Index()
         {
+            return View();
+        }
+        public ActionResult Log_in()
+        {
+            var userName = Request.Form["userName"].ToString();
+            var password = Request.Form["password"].ToString();
+            //compre with mongo
+            //success: load messages site
+            //fail: show message and form again
             return View();
         }
     }
