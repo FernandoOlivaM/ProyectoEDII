eb597a7140858717b6e14ac1abd78070d92041f5
feroliv4z <feroliv4z@gmail.com>
2019-11-24 18:59:42 -0600

    se creo vista y controlador para enviar mensajes

---
+++ ProyectoEDII/TeleCord/Controllers/MessagesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeleCord.Controllers
+{
+    public class MessagesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendMessage(HttpPostedFileBase postedFile)
+        {
+            var key = Convert.ToInt32(Request.Form["key"]);
+            var message = Request.Form["message"].ToString();
+
+            if (postedFile!=null)
+            {
+                //comprimir el archivo
+            }
+            else
+            {
+                //se trabaja solo con el mensaje
+            }
+            return View();
+        }
+        
+    }
+}
