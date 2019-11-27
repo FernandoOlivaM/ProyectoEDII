88f0e84afb95136ecceba691904e0786a982fcaf
MacheteZ5 <machetemontenegro@hotmail.com>
2019-11-26 22:30:49 -0600

    Se implementó la busqueda por Keyword

--- ProyectoEDII/TeleCord/Controllers/RecieveController.cs
+++ ProyectoEDII/TeleCord/Controllers/RecieveController.cs
@@ -94,5 +94,66 @@ namespace TeleCord.Controllers
             }
             return View(byteList);
         }
+
+        public ActionResult SearchbyKeyword(string Key, string ToUser, string KeyWord)
+        {
+            //recibir mensajes
+            var User = new Users();
+            var messages = User.GetMessages();
+            //decifrarlo
+            var StringList = new List<string>();
+            var ToRList = new List<string>();
+            foreach (MessagesElements elements in messages)
+            {
+                if ((elements.Transmitter == datosSingelton.Datos.Nombre) && (elements.Reciever == ToUser))
+                {
+                    StringList.Add(elements.text);
+                    ToRList.Add("1");
+                }
+                else
+                {
+                    if ((elements.Transmitter == ToUser) && (elements.Reciever == datosSingelton.Datos.Nombre))
+                    {
+                        StringList.Add(elements.text);
+                        ToRList.Add("0");
+                    }
+                }
+            }
+            //Decifrar cada mensaje con la llave que se recibió
+            SDES DecifradoSDES = new SDES();
+            var P10 = "8537926014";
+            var P8 = "79358216";
+            var P4 = "0321";
+            var EP = "01323210";
+            var IP = "63572014";
+            var ReverseIP = DecifradoSDES.GenerarIPInverso(IP);
+            //generar claves
+            var resultanteLS1 = DecifradoSDES.GenerarLS1(Key, P10);
+            var K1 = DecifradoSDES.GenerarK1(resultanteLS1, P8);
+            var K2 = DecifradoSDES.GenerarK2(resultanteLS1, P8);
+            var BinaryList = DecifradoSDES.LecutraArchivoDecifrar(StringList);
+            var byteList = new List<MessagesElements>();
+            var cifrar = false;
+            var counter = 0;
+            foreach (List<string> list in BinaryList)
+            {
+                var Message = new MessagesElements();
+                var response = string.Empty;
+                foreach (string binary in list)
+                {
+                    byte bytefinal = DecifradoSDES.CifrarODecifrar(binary, IP, EP, K1, P4, K2, ReverseIP, cifrar);
+                    response += (char)bytefinal;
+                }
+                Message.Transmitter = ToRList[counter] == "1" ? datosSingelton.Datos.Nombre : ToUser;
+                Message.Reciever = ToRList[counter] == "1" ? ToUser : datosSingelton.Datos.Nombre;
+                Message.text = response;
+                if(response.Contains(KeyWord))
+                {
+                    byteList.Add(Message);
+                }
+                counter++;
+            }
+            return View(byteList);
+        }
     }
 }
